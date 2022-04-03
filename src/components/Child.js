e11a830b519aee8ceb3418ff55b4899686b0bf73
PoojaDornal <37832052+PoojaDornal@users.noreply.github.com>
2022-04-03 15:45:29 +0530

    Update Child.js

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,8 +1,6 @@
 import React from "react";
 
-const Child = ()=>
-{
-    return <h2>Welcome to Newton School.</h2>
+const Child = () => {
+  return <h2>Welcome to Newton School.</h2>;
 };
 export default Child;
- 
