938c47e9345ad0de9a13bcf413042764bfb98ad4
PoojaDornal <37832052+PoojaDornal@users.noreply.github.com>
2022-04-03 15:47:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 import Welcome from "./Welcome";
 
 const App = () => {
-  return (
-    <Welcome name="" />
-          <h2>Welcome to  Newton School. </h2>
-    </>
-  );
+  return (<Welcome name="" />
+          <h2>Welcome to Newton School. </h2>
+         );
 };
-
 
 export default App;
